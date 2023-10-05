6cb2f44263d533b849f69fbebfd371a600291349
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2023-10-04 21:31:30 -0300

    feat: clear code

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 const AuthContext = createContext();
